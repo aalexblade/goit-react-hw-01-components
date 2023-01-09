71382be548f76ae38e341f7fbd3d6d86acfab404
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2023-01-09 23:11:53 +0200

    hw-01-user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+// import { username, tag, location, avatar, stats } from '../data/user.json';
+import { Profile } from './Profile/Profile'
+
+import user from '../data/user.json';
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+       <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
     </div>
   );
 };
